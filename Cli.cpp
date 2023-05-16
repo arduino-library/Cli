e7eb49b060f7fee162a4d89af545ebeefb55e8f2
Karim Hraibi <khraibi@apple.com>
2023-05-17 00:17:31 +0200

    Changed \n to \r\n for compatibility with screen

--- Cli.cpp
+++ Cli.cpp
@@ -60,7 +60,7 @@ int CliClass::newCmd (const char *name, const char *description, int(*function)(
   if (!initialized) return EXIT_FAILURE;
 
   if (this->numCmds >= CLI_NUM_CMD) {
-    xprintf("OVF!\n");
+    xprintf("OVF!\r\n");
     return EXIT_FAILURE;
   }
 
@@ -148,7 +148,7 @@ int CliClass::getCmd (void)
           return rv;
         }
       }
-      xprintf("Unknown '%s'\n", argv[0]);
+      xprintf("Unknown '%s'\r\n", argv[0]);
       break;
 
     default:
@@ -188,7 +188,7 @@ void CliClass::showHelp (void)
   //sortCmds ();
 
   Cli.xputs ("");
-  xprintf("Commands:\n");
+  xprintf("Commands:\r\n");
 
   // Display commands
   for (i = 0; (i < numCmds) && (i < CLI_NUM_CMD); i++) {
@@ -283,10 +283,12 @@ void CliClass::textPrintBlock (const char *text, int lineSize, int offset)
 
     // add new-line and padding
     if (*c != 0) {
+      xputchar('\r');
       xputchar('\n');
       for (i = 0; i < offset; i++) xputchar(' ');
     }
   }
+  xputchar('\r');
   xputchar('\n');
 }
 
@@ -314,7 +316,7 @@ void CliClass::xprintf (const char *fmt, ... ){
 void CliClass::xputs (const char *c){
   if (!initialized) return;
   Serial.print(c);
-  Serial.print("\n");
+  Serial.print("\r\n");
 }
 
 
